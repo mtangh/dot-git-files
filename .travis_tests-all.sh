e2c3d1bb1bc667b5fdcb6c17cae66e0cbe8da80c
MT <mtan.web+github@gmail.com>
2019-09-24 01:46:55 +0900

    Fix tests

--- .travis_tests-all.sh
+++ .travis_tests-all.sh
@@ -1,30 +1,40 @@
 #!/bin/bash -Cu
 THIS="${BASH_SOURCE##*/}"
-CDIR="${[ -n "${BASH_SOURCE%/*}" ] && cd "${BASH_SOURCE%/*}" &>/dev/null; pwd)
+CDIR=$([ -n "${BASH_SOURCE%/*}" ] && cd "${BASH_SOURCE%/*}" &>/dev/null; pwd)
 
-# Result
-tests_result=0
+# Status of all teats
+tests_stat=0
+# Ran tests
+tests_cseq=0
+# Result of test case
+tests_rval=0
 
 # Run tests
 for tests_sh in .travis.d/*.sh
 do
+
+  tests_cseq=$((++tests_cseq))
   xtrace_out="${tests_sh%.sh*}.xtrace.log"
   printf "#""%.s" {1..48}
   printf "\r%s " "${tests_sh##*/}"
   echo
   BASH_XTRACEFD=3 \
   bash -x "${tests_sh}" 3>"${xtrace_out}" || {
-    tests_result=$?
+    tests_rval=$?
     echo
-    echo "${tests_sh##*/}: Exit (${tests_result:-1})."
+    echo "${tests_sh##*/}: Exit (${tests_rval:-1})."
     echo "${tests_sh##*/}: XTRACE are:"
-    cat "${tests_xtrace_out}" 2>/dev/null
+    cat "${xtrace_out}" 2>/dev/null
     echo
     continue
   }
   echo
   echo "OK."
-done
+
+done &&
+[ ${tests_cseq:-0} -gt 0 ] &&
+[ ${tests_rval:-1} -eq 0 ]
+tests_stat=$?
 
 # End
-exit ${tests_result:-1}
+exit ${tests_stat:-1}
